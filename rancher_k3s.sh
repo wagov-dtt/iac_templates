6dd5b937c7fdb04779dd64c2d23fffa3afa30fa5
Adon Metcalfe <adonm@fastmail.fm>
2024-08-02 07:03:41 +0800

    better ordering

--- rancher_k3s.sh
+++ rancher_k3s.sh
@@ -10,10 +10,11 @@ curl -sfL https://get.k3s.io | INSTALL_K3S_CHANNEL=v1.28 sh -
 export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 
 curl -fsSL https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
-helm repo add rancher-stable https://releases.rancher.com/server-charts/stable
+
 helm repo add jetstack https://charts.jetstack.io --force-update
 helm install cert-manager jetstack/cert-manager --namespace cert-manager --create-namespace --set crds.enabled=true
 sleep 10
+helm repo add rancher-stable https://releases.rancher.com/server-charts/stable
 helm install rancher rancher-stable/rancher --namespace cattle-system --create-namespace --set hostname=$RANCHER_FQDN --set bootstrapPassword=$BOOTSTRAP_PASSWORD --set ingress.tls.source=letsEncrypt --set letsEncrypt.email=admin@$RANCHER_FQDN --set letsEncrypt.ingress.class=traefik
 sleep 10
 echo "Rancher installed at: https://$RANCHER_FQDN/dashboard/?setup=$BOOTSTRAP_PASSWORD"
