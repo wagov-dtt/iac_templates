4c40230ad5c2aa43d7c091d97f13bac0a49b0f3b
Adon Metcalfe <adonm@fastmail.fm>
2024-08-02 06:47:09 +0800

    tls

--- rancher_k3s.sh
+++ rancher_k3s.sh
@@ -9,12 +9,11 @@ BOOTSTRAP_PASSWORD=$(tr -dc 'a-zA-Z0-9' < /dev/urandom | fold -w 16 | head -n 1)
 curl -sfL https://get.k3s.io | INSTALL_K3S_CHANNEL=v1.28 sh -
 export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 
-kubectl create namespace cattle-system
 curl -fsSL https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 helm repo add rancher-stable https://releases.rancher.com/server-charts/stable
-
-kubectl apply -f https://github.com/cert-manager/cert-manager/releases/latest/download/cert-manager.yaml
-kubectl wait --for=condition=ready pod -l app.kubernetes.io/instance=cert-manager -n cert-manager --timeout=120s
-
-helm install rancher rancher-stable/rancher --namespace cattle-system --set hostname=$RANCHER_FQDN --set bootstrapPassword=$BOOTSTRAP_PASSWORD --set ingress.tls.source=letsEncrypt --set letsEncrypt.email=admin@$RANCHER_FQDN --set letsEncrypt.ingress.class=traefik
-echo "https://$RANCHER_FQDN/dashboard/?setup=$BOOTSTRAP_PASSWORD"
+helm repo add jetstack https://charts.jetstack.io --force-update
+helm install cert-manager jetstack/cert-manager --namespace cert-manager --create-namespace
+sleep 10
+helm install rancher rancher-stable/rancher --namespace cattle-system --create-namespace --set hostname=$RANCHER_FQDN --set bootstrapPassword=$BOOTSTRAP_PASSWORD --set ingress.tls.source=letsEncrypt --set letsEncrypt.email=admin@$RANCHER_FQDN --set letsEncrypt.ingress.class=traefik
+sleep 10
+echo "Rancher installed at: https://$RANCHER_FQDN/dashboard/?setup=$BOOTSTRAP_PASSWORD"
