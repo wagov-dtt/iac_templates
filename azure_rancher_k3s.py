f11bffd6b0a14ee6e6c5db8a94d31a6b318a7f3b
Adon Metcalfe <adonm@fastmail.fm>
2024-08-02 06:17:30 +0800

    drop identity as not needed

--- azure_rancher_k3s.py
+++ azure_rancher_k3s.py
@@ -21,7 +21,7 @@ fqdn = f"{dns_name}.{location}.cloudapp.azure.com"
 
 commands = [
     f"az group create -n {name} -l {location}",
-    f"az vm create -g {name} -n {name}-rancher --image {vm_image} --generate-ssh-keys --size {vm_size} --public-ip-sku Standard --public-ip-address-dns-name {dns_name} --assign-identity --role contributor --scope /subscriptions/$(az account show --query id -o tsv)/resourceGroups/{name} --os-disk-size-gb {os_disk_size}",
+    f"az vm create -g {name} -n {name}-rancher --image {vm_image} --generate-ssh-keys --size {vm_size} --public-ip-sku Standard --public-ip-address-dns-name {dns_name} --os-disk-size-gb {os_disk_size}",
     f"az network nsg rule create -g {name} --nsg-name {name}-rancherNSG -n AllowHTTP --priority 1010 --destination-port-ranges 80 --access Allow --protocol Tcp",
     f"az network nsg rule create -g {name} --nsg-name {name}-rancherNSG -n AllowHTTPS --priority 1020 --destination-port-ranges 443 --access Allow --protocol Tcp",
 ]
