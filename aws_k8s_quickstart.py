2117e6a969653b9febbb4ec159f9aeb4f13c9c3f
Adon Metcalfe <adonm@fastmail.fm>
2024-08-10 12:24:45 +0800

    mostly working, needs some fiddling with bottlerocket ephemeral stores

---
+++ aws_k8s_quickstart.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+
+import sys, os, subprocess, tempfile
+
+env = os.environ.get
+cwd = os.getcwd()
+name = sys.argv[1]
+
+eks_cluster = f"""
+apiVersion: eksctl.io/v1alpha5
+kind: ClusterConfig
+metadata:
+  name: {name}
+  region: {env('AWS_REGION', 'ap-southeast-2')}
+cloudWatch:
+  clusterLogging:
+    enableTypes: ["*"]
+addonsConfig:
+  disableDefaultAddons: true
+addons:
+  - name: coredns
+  - name: amazon-cloudwatch-observability
+  - name: aws-guardduty-agent
+iam:
+  withOIDC: true
+managedNodeGroups:
+- name: workers
+  instanceType: i4i.xlarge
+  minSize: 3
+  maxSize: 6
+  desiredCapacity: 3
+  privateNetworking: true
+  volumeSize: 64
+  maxPodsPerNode: 1000
+  amiFamily: Bottlerocket
+  spot: true
+"""
+
+def run(cmd: str) -> str:
+    print(cmd)
+    subprocess.run(cmd, shell=True)
+
+if __name__ == "__main__":
+    with tempfile.TemporaryDirectory() as tmpdir:
+        os.chdir(tmpdir)
+        print(eks_cluster)
+        open("eks-config.yaml", "w").write(eks_cluster.split("managedNodeGroups:")[0])
+        run("curl -sL https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_Linux_amd64.tar.gz | tar xz")
+        run("curl -sL https://github.com/cilium/cilium-cli/releases/latest/download/cilium-linux-amd64.tar.gz | tar xz")
+        run("./eksctl create cluster -f eks-config.yaml --without-nodegroup")
+        run(f"./cilium install --set kubeProxyReplacement=true --set k8sServiceHost=$(./eksctl get cluster --name {name} -o yaml | grep Endpoint: | cut -d'/' -f 3) --set k8sServicePort=443")
+        open("eks-config.yaml", "w").write(eks_cluster)
+        run(f"./eksctl create nodegroup -f eks-config.yaml")
+        run(f"./cilium hubble enable --ui")
+        run("./cilium status --wait")
+        run("./cilium connectivity test")
+
+        os.chdir(cwd)
