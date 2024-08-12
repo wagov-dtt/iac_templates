6d1928aff17f87b079ae68f4866d8722844dc3d4
Adon Metcalfe <adonm@fastmail.fm>
2024-08-12 22:22:36 +0800

    simplifying k3s on aws

--- rancher_setup/rancher_k3s.py
+++ rancher_setup/rancher_k3s.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
-import os, sys
+"""
+This script installs K3s and Rancher on a system (tested on Debian 12), configuring necessary parameters and dependencies.
+"""
+
+import os
+import sys
 from pathlib import Path
-from subprocess import call
+from subprocess import run, call
 
 def increase_limits():
+    """
+    Increase system limits for inotify instances, watches, and memory mappings.
+    Writes new limits to a sysctl configuration file and applies the changes.
+    """
     sysctl_params = """
 fs.inotify.max_user_instances = 8192
 fs.inotify.max_user_watches = 524288
@@ -13,6 +22,22 @@ vm.max_map_count = 262144
     call(["sysctl", "--system"])
 
 def main(rancher_fqdn, password):
+    """
+    Main function to install and configure K3s and Rancher.
+
+    Args:
+        rancher_fqdn (str): Fully Qualified Domain Name for Rancher
+        password (str): Bootstrap password for Rancher
+
+    This function performs the following steps:
+    1. Increases system limits
+    2. Installs K3s
+    3. Sets up Helm
+    4. Installs cert-manager
+    5. Installs Rancher
+    """
+    if rancher_fqdn == "sslip.io":
+        rancher_fqdn = run("dig @ns.sslip.io txt ip.sslip.io +short -4", shell=True, text=True).stdout.replace('"', "").strip() + ".sslip.io"
     increase_limits()
     call('curl -sfL https://get.k3s.io | INSTALL_K3S_CHANNEL=v1.28 INSTALL_K3S_EXEC="--kubelet-arg=max-pods=900" sh -', shell=True)
     os.environ["KUBECONFIG"] = "/etc/rancher/k3s/k3s.yaml"
@@ -24,8 +49,6 @@ def main(rancher_fqdn, password):
     call(["helm", "install", "rancher", "rancher-stable/rancher", "--namespace", "cattle-system", "--create-namespace",
          "--set", f"hostname={rancher_fqdn}", "--set", f"bootstrapPassword={password}", "--set", "ingress.tls.source=letsEncrypt",
          "--set", f"letsEncrypt.email=admin@{rancher_fqdn}", "--set", "letsEncrypt.ingress.class=traefik"])
-    call(["sleep", "30"])
-    print(f"Rancher installed at: https://{rancher_fqdn}/dashboard/?setup={password}")
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
