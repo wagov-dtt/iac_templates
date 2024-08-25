5a89bf603497d3ed06c8e497125db2f43aa9bc7f
Adon Metcalfe <adonm@fastmail.fm>
2024-08-25 12:00:42 +0800

    all seems to work yay ipv6

--- eksctl_quickstart.py
+++ eksctl_quickstart.py
@@ -9,7 +9,7 @@ region = os.environ.get("AWS_REGION", "ap-southeast-2")
 tags = os.environ.get("TAGS", "environment=dev,project=demo")
 node_groups = os.environ.get("NODE_GROUPS", "base:2,scale-spot:1")
 instance_type = os.environ.get("INSTANCE_TYPE", "i4i.2xlarge")
-include_rancher = os.environ.get("RANCHER", "true")
+rancher_fqdn = os.environ.get("RANCHER_FQDN", False)
 
 
 def install_tools():
@@ -83,14 +83,62 @@ def generate_config(temp_dir):
 
     return config_path
 
+traefik_config = """service:
+  annotations:
+    service.beta.kubernetes.io/aws-load-balancer-type: external
+    service.beta.kubernetes.io/aws-load-balancer-nlb-target-type: ip
+    service.beta.kubernetes.io/aws-load-balancer-scheme: internet-facing
+    service.beta.kubernetes.io/aws-load-balancer-ip-address-type: dualstack
+  spec:
+    externalTrafficPolicy: Local
+
+providers:
+  kubernetesCRD:
+    enabled: true
+  kubernetesIngress:
+    enabled: true
+    publishedService:
+      enabled: true
+
+ingressClass:
+  enabled: true
+  isDefaultClass: true
+
+additionalArguments:
+  - "--providers.kubernetesingress.ingressclass=traefik"
+  - "--providers.kubernetesingress.allowexternalnameservices=true"
+"""
+
+rancher_cmds = [
+    f"eksctl utils write-kubeconfig --cluster={cluster_name} --region={region}",
+    "curl -o iam-policy.json https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/v2.8.2/docs/install/iam_policy.json",
+    "aws iam create-policy --policy-name AWSLoadBalancerControllerIAMPolicy --policy-document file://iam-policy.json",
+    (f"eksctl create iamserviceaccount --cluster={cluster_name} --region={region} --namespace=kube-system --name=aws-load-balancer-controller"
+     " --attach-policy-arn=arn:aws:iam::$(aws sts get-caller-identity --query Account --output text):policy/AWSLoadBalancerControllerIAMPolicy"
+     " --override-existing-serviceaccounts --approve"),
+    "helm repo add eks https://aws.github.io/eks-charts",
+    "helm repo add traefik https://traefik.github.io/charts",
+    "helm repo add jetstack https://charts.jetstack.io",
+    "helm repo add rancher-latest https://releases.rancher.com/server-charts/latest",
+    "helm repo update",
+    f"helm install aws-load-balancer-controller eks/aws-load-balancer-controller -n kube-system --set clusterName={cluster_name} --set serviceAccount.create=false --set serviceAccount.name=aws-load-balancer-controller",
+    "helm install traefik traefik/traefik -n kube-system -f traefik-values.yaml",
+    "helm install cert-manager jetstack/cert-manager --namespace cert-manager --create-namespace --set crds.enabled=true",
+    ("helm install rancher rancher-latest/rancher --create-namespace --namespace cattle-system --set ingress.tls.source=letsEncrypt"
+     f" --set letsEncrypt.ingress.class=traefik --set hostname={rancher_fqdn} --set letsEncrypt.email={rancher_fqdn}@maildrop.cc")
+]
+
 
 def main():
     with tempfile.TemporaryDirectory() as temp_dir:
         os.chdir(temp_dir)
         install_tools()
-        run(f"PATH={temp_dir}:$PATH eksctl create cluster -f {generate_config(temp_dir)}", shell=True)
-        if include_rancher:
-            "TODO: use helm to setup rancher and traefik"
+        cmds = [f"eksctl create cluster -f {generate_config(temp_dir)}"]
+        if rancher_fqdn:
+            cmds += rancher_cmds
+            Path("traefik-values.yaml").write_text(traefik_config)
+        script = "\n".join(cmds)
+        run(f"export PATH={temp_dir}:$PATH\n{script}", shell=True)
 
 
 if __name__ == "__main__":
